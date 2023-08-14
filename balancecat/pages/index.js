e55d322b885724115f1557917762f52b9844c67e
lcy-lin <linjoy6@gmail.com>
2023-08-14 17:22:00 +0800

    test(navbar): test

--- balancecat/pages/index.js
+++ balancecat/pages/index.js
@@ -4,7 +4,7 @@ import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
-// test
+// test2
 
 export default function Home() {
   return (
