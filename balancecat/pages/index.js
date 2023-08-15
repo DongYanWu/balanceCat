2e9cad1e49d180e183f6de98f78d36102de943ca
lcy-lin <linjoy6@gmail.com>
2023-08-15 15:39:58 +0800

    feat(dateselect): create component

--- balancecat/pages/index.js
+++ balancecat/pages/index.js
@@ -1,13 +1,15 @@
 import FrequentlyAskedQuestions from "@/components/FrequentlyAskedQuestions";
-import AccountsBox from "@/components/AccountsBox";
+// import AccountsBox from "@/components/AccountsBox";
 import AssetsOverview from "@/components/AssetsOverview";
+import DateSelector from "@/components/DateSelector";
 import NavBar from "../components/NavBar";
 
 export default function Home() {
   return (
     <div>
       <NavBar />
-      <AccountsBox />
+      {/* <AccountsBox /> */}
+      <DateSelector />
       <AssetsOverview />
       <FrequentlyAskedQuestions />
     </div>
