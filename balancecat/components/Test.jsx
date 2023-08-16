77ab6a1ac5045f5655d66a172f1b37623154817d
DongYanWu <janetony908@gmail.com>
2023-08-16 16:48:50 +0800

    feat(add the cal component): add in the test.jsx

--- balancecat/components/Test.jsx
+++ balancecat/components/Test.jsx
@@ -1,113 +1,85 @@
-// // pages/index.js
-// import React from "react";
-// import {
-//   Table,
-//   TableBody,
-//   TableCell,
-//   TableContainer,
-//   TableHead,
-//   TableRow,
-//   Paper,
-//   Collapse,
-//   IconButton,
-// } from "@mui/material";
+import * as React from "react";
+import dayjs from "dayjs";
+import isBetweenPlugin from "dayjs/plugin/isBetween";
+import { styled } from "@mui/material/styles";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
+import { PickersDay } from "@mui/x-date-pickers/PickersDay";
 
-// import ExpandLessIcon from "@mui/icons-material/ExpandLess";
-// import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+dayjs.extend(isBetweenPlugin);
 
-// const data = [
-//   {
-//     name: "Assets",
-//     children: [
-//       {
-//         name: "Current Assets",
-//         value: 1000,
-//         lastMonthValue: 800,
-//         children: [
-//           { name: "Cash", value: 500, lastMonthValue: 900 },
-//           { name: "Accounts Receivable", value: 500, lastMonthValue: 900 },
-//         ],
-//       },
-//       {
-//         name: "Non-Current Assets",
-//         value: 1500,
-//         lastMonthValue: 900,
-//         children: [{ name: "Property", value: 1500, lastMonthValue: 900 }],
-//       },
-//     ],
-//   },
-//   {
-//     name: "Liabilities",
-//     children: [
-//       {
-//         name: "Current Liabilities",
-//         value: 500,
-//         lastMonthValue: 900,
-//         children: [
-//           { name: "Short Term Debt", value: 500, lastMonthValue: 900 },
-//         ],
-//       },
-//       {
-//         name: "Non-Current Liabilities",
-//         value: 1000,
-//         lastMonthValue: 900,
-//         children: [
-//           { name: "Long Term Debt", value: 1000, lastMonthValue: 900 },
-//         ],
-//       },
-//     ],
-//   },
-// ];
+const CustomPickersDay = styled(PickersDay, {
+  shouldForwardProp: (prop) =>
+    prop !== "dayIsBetween" && prop !== "isFirstDay" && prop !== "isLastDay",
+})(({ theme, dayIsBetween, isFirstDay, isLastDay }) => ({
+  ...(dayIsBetween && {
+    borderRadius: 0,
+    backgroundColor: theme.palette.primary.main,
+    color: theme.palette.common.white,
+    "&:hover, &:focus": {
+      backgroundColor: theme.palette.primary.dark,
+    },
+  }),
+  ...(isFirstDay && {
+    borderTopLeftRadius: "50%",
+    borderBottomLeftRadius: "50%",
+  }),
+  ...(isLastDay && {
+    borderTopRightRadius: "50%",
+    borderBottomRightRadius: "50%",
+  }),
+}));
 
-// export default function Test() {
-//   const [open, setOpen] = React.useState({});
+function Day(props) {
+  const { day, startDate, endDate, ...other } = props;
 
-//   const handleToggle = (name) => {
-//     setOpen((prev) => ({ ...prev, [name]: !prev[name] }));
-//   };
+  if (!startDate || !endDate) {
+    return <PickersDay day={day} {...other} />;
+  }
 
-//   const renderRows = (items, depth = 0) =>
-//     items.map((item) => (
-//       <React.Fragment key={item.name}>
-//         <TableRow>
-//           <TableCell style={{ paddingLeft: `${depth * 20}px` }}>
-//             {item.children && (
-//               <IconButton size="small" onClick={() => handleToggle(item.name)}>
-//                 {open[item.name] ? <ExpandLessIcon /> : <ExpandMoreIcon />}
-//               </IconButton>
-//             )}
-//             {item.name}
-//           </TableCell>
+  const dayIsBetween = day.isBetween(startDate, endDate, null, "[]");
+  const isFirstDay = day.isSame(startDate, "day");
+  const isLastDay = day.isSame(endDate, "day");
 
-//           <TableCell>{item.value}</TableCell>
-//           <TableCell align="right">{item.lastMonthValue}</TableCell>
-//         </TableRow>
-//         {item.children &&
-//           open[item.name] &&
-//           renderRows(item.children, depth + 1)}
-//       </React.Fragment>
-//     ));
+  return (
+    <CustomPickersDay
+      {...other}
+      day={day}
+      sx={dayIsBetween ? { px: 2.5, mx: 0 } : {}}
+      dayIsBetween={dayIsBetween}
+      isFirstDay={isFirstDay}
+      isLastDay={isLastDay}
+    />
+  );
+}
 
-//   return (
-//     <TableContainer
-//       component={Paper}
-//       style={{ border: "1px solid black", width: "80%", margin: "auto" }}
-//     >
-//       <Table aria-label="nested table">
-//         <TableHead>
-//           <TableRow>
-//             <TableCell>Category</TableCell>
-//             <TableCell
-//               align="right"
-//               style={{ paddingRight: 4, paddingLeft: 4 }}
-//             >
-//               Value
-//             </TableCell>
-//             <TableCell align="right">Last Month Value</TableCell>
-//           </TableRow>
-//         </TableHead>
-//         <TableBody>{renderRows(data)}</TableBody>
-//       </Table>
-//     </TableContainer>
-//   );
-// }
+export default function Test() {
+  const [dateRange, setDateRange] = React.useState([null, null]);
+
+  const handleDateChange = (date) => {
+    if (!dateRange[0]) {
+      setDateRange([date, null]);
+    } else if (!dateRange[1]) {
+      setDateRange([dateRange[0], date]);
+    } else {
+      setDateRange([date, null]);
+    }
+  };
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DateCalendar
+        value={dateRange[1] || dateRange[0]}
+        onChange={handleDateChange}
+        slots={{ day: Day }}
+        slotProps={{
+          day: {
+            startDate: dateRange[0],
+            endDate: dateRange[1],
+          },
+        }}
+      />
+    </LocalizationProvider>
+  );
+}
---
+++ balancecat/pages/userpage/index.js
@@ -0,0 +1,5 @@
+import Test from "@/components/Test";
+
+export default function UserPage() {
+  return <Test />;
+}
