30ef5bc37101869fe2540215c25b9451855fdb41
lcy-lin <linjoy6@gmail.com>
2023-08-16 16:45:18 +0800

    feat(detail): change theme

--- balancecat/pages/accountdetail/index.js
+++ balancecat/pages/accountdetail/index.js
@@ -1,4 +1,7 @@
 import { Handjet, DotGothic16 } from "next/font/google";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import NavBar from "@/components/NavBar";
 import SubjectDetail from "@/components/SubjectDetial";
 import styles from "./detail.module.scss";
@@ -11,12 +14,31 @@ const handjet = Handjet({
   weight: "400",
   subsets: ["latin"],
 });
-export default function BalanceSheetPage() {
+export default function DetailPage() {
   return (
     <div className={styles.wrapper}>
       <NavBar />
       <p className={`${dot.className} ${styles.title}`}>現金</p>
       <p className={`${handjet.className} ${styles.subtitle}`}>cash</p>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DatePicker
+          // label={'"year", "month" and "day"'}
+          views={["year", "month", "day"]}
+          sx={{ backgroundColor: "#36364e", borderRadius: "10px" }}
+        />
+      </LocalizationProvider>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DatePicker
+          // label={'"year", "month" and "day"'}
+          views={["year", "month", "day"]}
+          sx={{
+            backgroundColor: "#36364e",
+            borderRadius: "10px",
+            color: "white",
+          }}
+        />
+      </LocalizationProvider>
+
       <SubjectDetail />
     </div>
   );
