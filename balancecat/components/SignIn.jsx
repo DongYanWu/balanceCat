71ea6269751b370138d8e2732acab1148deef912
DongYanWu <janetony908@gmail.com>
2023-08-21 10:29:55 +0800

    feat(addsignup api): add api

--- balancecat/components/SignIn.jsx
+++ balancecat/components/SignIn.jsx
@@ -1,9 +1,74 @@
 import { Button } from "@mui/material";
+import useSWRMutation from "swr/mutation";
+import swal from "sweetalert";
+import cookieCutter from "cookie-cutter";
+import { useRouter } from "next/router";
+import { useState } from "react";
 import styles from "@/styles/signup.module.scss";
 import PwdInput from "./PwdInput";
 import InputBox from "./InputBox";
 
-export default function SignUp({ setIsLogIn }) {
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+async function sendRequest(url, { arg }) {
+  console.log(arg);
+  return fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(arg),
+  })
+    .then((response) => response)
+    .then((data) => [data, data.json()])
+
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+
+export default function SignIn({ setIsLogIn }) {
+  const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { trigger, isMutating } = useSWRMutation(
+    `${API_URL}users/signin`,
+    sendRequest,
+  );
+
+  const handleSignIn = () => {
+    // Here you can call your POST API with the collected data
+    trigger({ email, password }).then(async (data) => {
+      const response = data[0];
+      const userData = await data[1];
+      if (response.status === 403) {
+        swal(
+          "Error",
+          "Wrong Password, User Not Found, Wrong provider",
+          "error",
+        );
+      }
+      if (response.status === 400) {
+        swal("Error", "Client Error Response", "error");
+      }
+      if (response.status === 500) {
+        swal(
+          "Error",
+          "Something's wrong. Please try again later or notify our engineering team",
+          "info",
+        );
+      }
+      if (response.status === 200) {
+        // setIsLoginPage(true);
+        router.push("/");
+
+        // Cookies.set('token', userData.data.access_token, { secure: true, httpOnly: true });
+        cookieCutter.set("id", userData.data.user.id);
+        cookieCutter.set("token", userData.data.access_token);
+      }
+    });
+  };
   return (
     <div className={styles.wrapper}>
       <div className={styles.title_container}>
@@ -30,11 +95,12 @@ export default function SignUp({ setIsLogIn }) {
       </div>
       <div className={styles.inputs}>
         <br />
-        <InputBox email />
+        <InputBox isEmail email={email} setEmail={setEmail} />
         <br />
-        <PwdInput />
+        <PwdInput password={password} setPassword={setPassword} />
         <br />
         <Button
+          onClick={handleSignIn}
           variant="contained"
           sx={{
             fontWeight: "600",
--- balancecat/components/SignUp.jsx
+++ balancecat/components/SignUp.jsx
@@ -1,104 +1,111 @@
-import Image from "next/image";
-import { Silkscreen } from "next/font/google";
-import FrequentlyAskedQuestions from "@/components/FrequentlyAskedQuestions";
-import AssetsOverview from "@/components/assetsOverview/AssetsOverview";
-import Entries from "@/components/entriesForm/Entries";
-import Footer from "@/components/Footer";
-import styles from "@/styles/home.module.scss";
-import CardTemplate from "@/components/cardTemplate/CardTemplate";
-import SideBar from "@/components/SideBar";
+import { Button } from "@mui/material";
+import useSWRMutation from "swr/mutation";
+import { useState } from "react";
+import swal from "sweetalert";
+import styles from "@/styles/signup.module.scss";
+import PwdInput from "./PwdInput";
+import InputBox from "./InputBox";
 
-const silk = Silkscreen({
-  weight: ["400"],
-  subsets: ["latin"],
-});
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
-export default function Home() {
+async function sendRequest(url, { arg }) {
+  return fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(arg),
+  })
+    .then((response) => response)
+    .then((data) => [data, data.json()])
+
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+
+export default function SignUp({ setIsLogIn }) {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const { trigger, isMutating } = useSWRMutation(
+    `${API_URL}users/signup`,
+    sendRequest,
+  );
+
+  const handleSignUp = () => {
+    // Here you can call your POST API with the collected data
+    console.log(email, username, password, confirmPassword);
+    trigger({ name: username, email, password }).then(async (data) => {
+      const response = data[0];
+      // const userData = await data[1]; //it would it you the user data(token user_id and provider which now is native)
+      if (response.status === 403) {
+        swal("Error", "Email Already Exists", "error");
+      }
+      if (response.status === 400) {
+        swal("Error", "Client Error Response", "error");
+      }
+      if (response.status === 500) {
+        swal(
+          "Error",
+          "Something's wrong. Please try again later or notify our engineering team",
+          "info",
+        );
+      }
+      if (response.status === 200) {
+        setIsLogIn(true);
+      }
+    });
+  };
+
   return (
     <div className={styles.wrapper}>
-      <CardTemplate
-        backgroundStyle={{
-          background: "linear-gradient(180deg, #0d0221 0%, #090630 100%)",
-        }}
-        style={{
-          display: "flex",
-          background: "linear-gradient(to bottom right, #fff, #acb5c2)",
-          boxShadow: "0 0 40px rgba(255, 255, 255, 1)",
-          border: "none",
-        }}
-      >
-        <SideBar />
-        <div className={styles.box}>
-          <div className={styles.title_entries}>
-            <p className={`${silk.className} ${styles.title}`}>You always</p>
-            <p className={`${silk.className} ${styles.title}`}>are winner</p>
-            <Entries />
-          </div>
-        </div>
-        <Image
-          src="/astrocat.png"
-          alt="cat pic"
-          width={300}
-          height={300}
-          className={styles.cat}
-          style={{
-            borderRadius: "30%",
-            // position: "absolute",
-            // bottom: "20px",
-            // left: "50%",
-            transform: "translateX(-50%)",
-            animation: "bounce 2s infinite",
+      <div className={styles.title_container}>
+        <p className={styles.circle} />
+        <p className={styles.title}>Balance Cat</p>
+      </div>
+
+      <p className={styles.free}>START FOR FREE</p>
+      <div className={styles.sentence}>
+        <p className={styles.new}>Create new account</p>
+        <p className={styles.period}>.</p>
+      </div>
+      <div className={styles.member_login}>
+        <p className={styles.member}>Already A Member?</p>
+        <p
+          className={styles.login}
+          onClick={() => {
+            setIsLogIn(true);
           }}
+          aria-hidden="true"
+        >
+          Log In
+        </p>
+      </div>
+      <div className={styles.inputs}>
+        <InputBox isEmail email={email} setEmail={setEmail} />
+        <InputBox isUsername username={username} setUsername={setUsername} />
+        <PwdInput password={password} setPassword={setPassword} />
+        <PwdInput
+          confirm
+          confirmPassword={confirmPassword}
+          setConfirmPassword={setConfirmPassword}
         />
-      </CardTemplate>
-      <style>
-        {`
-          /* 定義彈跳動畫 */
-          @keyframes bounce {
-            0%,
-            100% {
-              transform: translateY(0); /* 初始和結束狀態為不移動 */
-            }
-            50% {
-              transform: translateY(-20px); /* 中間狀態為向上移動 */
-            }
-          }
-        `}
-      </style>
-
-      <CardTemplate
-        backgroundStyle={{
-          background: "linear-gradient(180deg, #0d0221 0%, #090630 100%)",
-        }}
-        style={{
-          display: "flex",
-          gap: "2rem",
-          background: "linear-gradient(to bottom right, #fff, #acb5c2)",
-          boxShadow: "0 0 40px rgba(255, 255, 255, 1)",
-          border: "none",
-        }}
-      >
-        <SideBar />
-        <AssetsOverview />
-      </CardTemplate>
-
-      <CardTemplate
-        backgroundStyle={{
-          background: "linear-gradient(180deg, #0d0221 0%, #090630 100%)",
-        }}
-        style={{
-          display: "flex",
-          background: "linear-gradient(to bottom right, #fff, #acb5c2)",
-          boxShadow: "0 0 40px rgba(255, 255, 255, 1)",
-          border: "none",
-        }}
-      >
-        <SideBar />
-        <div style={{ alignSelf: "center", width: "100%" }}>
-          <FrequentlyAskedQuestions />
-        </div>
-      </CardTemplate>
-      <Footer />
+        <br />
+        <Button
+          onClick={handleSignUp}
+          variant="contained"
+          sx={{
+            width: "20ch",
+            backgroundColor: "#4481f4",
+            borderRadius: "15px",
+          }}
+        >
+          Sign Up
+        </Button>
+      </div>
     </div>
   );
 }
