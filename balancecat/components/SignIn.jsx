8f843339f7cb34b5d5f58cb8ba825b5e30e6ca4c
DongYanWu <janetony908@gmail.com>
2023-08-23 12:01:34 +0800

    fix(fix): fix the lax and package

--- balancecat/components/SignIn.jsx
+++ balancecat/components/SignIn.jsx
@@ -68,13 +68,9 @@ export default function SignIn({ setIsLogIn }) {
 
         // Cookies.set('token', userData.data.access_token, { secure: true, httpOnly: true });
 
-        cookieCutter.set("id", userData.data.user.id, { sameSite: "lax" });
-        cookieCutter.set("token", userData.data.access_token, {
-          sameSite: "lax",
-        });
-        cookieCutter.set("username", userData.data.user.name, {
-          sameSite: "lax",
-        });
+        cookieCutter.set("id", userData.data.user.id);
+        cookieCutter.set("token", userData.data.access_token);
+        cookieCutter.set("username", userData.data.user.name);
         router.push("/");
       }
     });
